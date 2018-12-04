61b833bea67584264b6a0258988e0495f5979f4b
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-12-04 15:10:11 +0100

    modified main file

--- src/LatPhysPlottingPyPlot.jl
+++ src/LatPhysPlottingPyPlot.jl
@@ -1,5 +1,27 @@
+################################################################################
+#
+#   module LatPhysPlottingPyPlot
+#   -> PyPlot
+#   -> LatPhysBase
+#   -> LinearAlgebra
+#
+#   --> PLOTTING OF LATTICES WITH PYPLOT
+#           - 2D lattices
+#           - 3D lattices
+#
+################################################################################
+
+
+# start of module
 module LatPhysPlottingPyPlot
 
-greet() = print("Hello World!")
 
-end # module
+# used libraries
+using LatPhysBase
+using PyPlot
+
+
+
+
+# end of module
+end
