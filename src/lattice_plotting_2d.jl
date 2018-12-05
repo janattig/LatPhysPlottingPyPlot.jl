b55d6ca2663880dd81df1674a6432f8f7fc2b09f
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-12-05 12:32:45 +0100

    created functions to wrap lattice plotting

---
+++ src/lattice_plotting_2d.jl
@@ -0,0 +1,31 @@
+# generic lattice plotting in 2D (with fance everything)
+function plotLattice(
+    		lattice :: L
+            ;
+            figsize :: Tuple{<:Real, <:Real} = (8,5),
+            coordinate_system :: Bool = false,
+            kwargs...
+        ) where {LS,LB,N,S<:AbstractSite{LS,2},B<:AbstractBond{LB,N},U,L<:AbstractLattice{S,B,U}}
+
+    # create a new figure
+    fig = PyPlot.figure(figsize = figsize)
+
+    # plot the lattice
+    plot(lattice; kwargs...)
+
+    # get the current axis
+    ax = fig[:gca]()
+    # turn off everthing but the plot
+    if !coordinate_system
+        ax[:set_axis_off]()
+    end
+    # equal aspect ratio
+    ax[:set_aspect]("equal")
+    # tighten layout
+    tight_layout()
+    # show
+    show()
+end
+
+# export plotting function
+export plotLattice
---
+++ src/lattice_plotting_3d.jl
@@ -0,0 +1,31 @@
+# generic lattice plotting in 3D (with fancy everything)
+function plotLattice(
+    		lattice :: L
+            ;
+            figsize :: Tuple{<:Real, <:Real} = (6,6),
+            coordinate_system :: Bool = false,
+            kwargs...
+        ) where {LS,LB,N,S<:AbstractSite{LS,3},B<:AbstractBond{LB,N},U,L<:AbstractLattice{S,B,U}}
+
+    # create a new figure
+    fig = PyPlot.figure(figsize = figsize)
+
+    # plot the lattice
+    plot(lattice; kwargs...)
+
+    # get the current axis
+    ax = fig[:gca]()
+    # turn off everthing but the plot
+    if !coordinate_system
+        ax[:set_axis_off]()
+    end
+    # equal aspect ratio
+    ax[:set_aspect]("equal")
+    # tighten layout
+    tight_layout()
+    # show
+    show()
+end
+
+# export plotting function
+export plotLattice
